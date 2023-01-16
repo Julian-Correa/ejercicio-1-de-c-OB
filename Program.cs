1564d4dc770f6e28a8021840f6b3c84793c51fda
dessinger <juliancorrea1403@gmail.com>
2023-01-16 13:25:02 -0300

    ejercicio de OB

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// ejercio 1
+Console.WriteLine("cual es tu nombre?");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hello" + " " + nombre);
+
+
+//ejercicio 2
+
+Console.WriteLine (DateTime.Now);
